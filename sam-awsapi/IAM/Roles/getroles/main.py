9c9c350ef28acd00627c0ed800642849cb5d1970
darshan <dar.raul@gmail.com>
2019-03-15 21:33:06 +0530

    added IAM roles functions

---
+++ sam-awsapi/IAM/Roles/getroles/main.py
@@ -0,0 +1,37 @@
+
+
+import boto3
+import firebase_auth as fa
+import json
+
+def lambda_handler(event, context):
+    profile_name=str(event['queryStringParameters']['profile'])
+    
+    #profile_name = str(event["profile"])
+    #profile_name = str(event['params']['querystring']['profile'])
+    ref = fa.getReference(profile_name)
+
+    iam=boto3.client('iam', region_name=str(ref.get()['region']), aws_access_key_id=str(ref.get()['access_key']), aws_secret_access_key=str(ref.get()['secret_access_key']))
+    roles=iam.list_roles()
+    roleslist=[]
+        
+    for role in roles['Roles']:
+            
+        name=role['RoleName']
+        description=role['Description']
+        roleslist.append({"Name":name,"Description":description})
+            
+        
+            
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps({
+            "roles":roleslist
+        }),
+        "isBase64Encoded": False
+    }
