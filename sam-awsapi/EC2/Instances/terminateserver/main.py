ad8a55b73d08126da08764a32833d3da0886f46e
darshan <dar.raul@gmail.com>
2019-03-15 22:07:38 +0530

    added EC2 instances/keypairs functions

---
+++ sam-awsapi/EC2/Instances/terminateserver/main.py
@@ -0,0 +1,27 @@
+
+
+import boto3
+import firebase_auth as fa
+import json
+
+def lambda_handler(event, context):
+    profile_name=str(event['queryStringParameters']['profile'])
+    instance_id=str(event['pathParameters']['instance_id'])
+    #profile_name = str(event["profile"])
+    #profile_name = str(event['params']['querystring']['profile'])
+    ref = fa.getReference(profile_name)
+
+    ec2=boto3.client('ec2', region_name=str(ref.get()['region']), aws_access_key_id=str(ref.get()['access_key']), aws_secret_access_key=str(ref.get()['secret_access_key']))
+    ec2.terminate_instances(InstanceIds=[str(instance_id)])
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps({
+            "server Terminated":instance_id
+        }),
+        "isBase64Encoded": False
+    }
---
+++ sam-awsapi/EC2/Keypairs/getkeypairs/main.py
@@ -0,0 +1,35 @@
+
+
+import boto3
+import firebase_auth as fa
+import json
+
+def lambda_handler(event, context):
+    profile_name=str(event['queryStringParameters']['profile'])
+    #profile_name = str(event["profile"])
+    #profile_name = str(event['params']['querystring']['profile'])
+    ref = fa.getReference(profile_name)
+
+    ec2=boto3.client('ec2', region_name=str(ref.get()['region']), aws_access_key_id=str(ref.get()['access_key']), aws_secret_access_key=str(ref.get()['secret_access_key']))
+    keypairdict={}
+    keypairlist=[]
+    keypairs=ec2.describe_key_pairs()
+    for keypair in keypairs['KeyPairs']:
+        keypairlist.append(keypair['KeyName'])
+    
+            
+    
+    
+    
+    #print(buckets)
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps({
+            "keypairs":keypairlist
+        }),
+        "isBase64Encoded": False
+    }
